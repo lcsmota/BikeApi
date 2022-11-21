8b14738a01943f61a0ee2967c1863c061dc8c2ea
lcsmota <sacul_m@protonmail.com>
2022-11-21 11:39:22 -0300

    Register DI Container

--- Program.cs
+++ Program.cs
@@ -1,5 +1,14 @@
+using BikeApi.Context;
+using BikeApi.Contracts;
+using BikeApi.Repository;
+
 var builder = WebApplication.CreateBuilder(args);
 {
+    builder.Services.AddSingleton<DapperContext>();
+    builder.Services.AddScoped<IProductRepository, ProductRepository>();
+    builder.Services.AddScoped<IBrandRepository, BrandRepository>();
+    builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+
     builder.Services.AddControllers();
 
     builder.Services.AddEndpointsApiExplorer();
